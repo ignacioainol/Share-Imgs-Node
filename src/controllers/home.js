c36b9c7e418c4eda8eace667a5f8be431c24645f
Ignacio <shoik.xtazy@gmail.com>
2019-06-30 01:45:01 -0400

    show images in home

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,11 @@
 const ctrl = {};
 
-ctrl.index = (req,res) => {
-	res.render("index");
+const { Image } = require('../models');
+
+ctrl.index = async (req,res) => {
+	const images = await Image.find().sort({ timestamp: -1 });
+	await Image.find();
+	res.render("index",{images: images });
 };
 
 module.exports = ctrl;
