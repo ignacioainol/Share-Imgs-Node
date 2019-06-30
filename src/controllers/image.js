5231daa42da08699415457ea46ea235243d04531
Ignacio <shoik.xtazy@gmail.com>
2019-06-30 01:02:29 -0400

    saveImage recursive

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -10,23 +10,41 @@ ctrl.index = (req,res) => {
 
 };
 
-ctrl.create = async (req,res) => {
-    const imgUrl = randomNumber();
-    console.log(imgUrl);
-    const imageTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    const targetPath = path.resolve('src/public/upload/'+imgUrl + ext);
+ctrl.create =  (req,res) => {
 
-    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
-        await fs.rename(imageTempPath, targetPath);
-        const newImage = new Image({
-            title: req.body.title,
-            filename: imgUrl + ext,
-            description: req.body.description
-        });
-        console.log(newImage);
-    }
-    res.send("works!");
+    const saveImage = async () => {
+        const imgUrl = randomNumber();
+        const images = await Image.find({filename: imgUrl});
+        if(images.length > 0){
+            saveImage();   
+        }else{
+            console.log(imgUrl);
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const targetPath = path.resolve('src/public/upload/'+imgUrl + ext);
+
+            if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+                await fs.rename(imageTempPath, targetPath);
+                const newImage = new Image({
+                    title: req.body.title,
+                    filename: imgUrl + ext,
+                    description: req.body.description
+                });
+                console.log(newImage);
+                const imageSaved = await newImage.save();
+                //res.redirect('/images/:image_id');
+                res.send('works');
+            }else{
+                await fs.unlink(imageTempPath);
+                res.status(500).json({error:'Only images are allowed'});
+            }
+
+        }
+    };
+
+    saveImage();
+
+    
 };
 
 ctrl.like = (req,res) => {
