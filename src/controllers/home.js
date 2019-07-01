bf154e92506b63fe9c0a0bd0e6e2b09a06d5673f
Ignacio <shoik.xtazy@gmail.com>
2019-07-01 17:14:36 -0400

    added stat of image

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -2,9 +2,15 @@ const ctrl = {};
 
 const { Image } = require('../models');
 
+ const sidebar = require('../helpers/sidebar');
+
 ctrl.index = async (req,res) => {
 	const images = await Image.find().sort({timestamp: -1});
-	res.render("index",{ images });
+	let viewModel = {images: [] };
+	viewModel.images = images;
+	await sidebar(viewModel);
+	viewModel = await sidebar(viewModel);
+	res.render("index", viewModel);
 };
 
 module.exports = ctrl;
