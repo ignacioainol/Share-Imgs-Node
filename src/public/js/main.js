b40682f0b8e5645210acb07adf20ebd868056bc2
Ignacio <shoik.xtazy@gmail.com>
2019-07-01 01:31:20 -0400

    btn delete image ok

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -9,3 +9,21 @@ $('#btn-like').on('click',function(e){
 
     return false;
 });
+
+$('#btn-delete').on('click',function(e){
+    let $this = $(this);
+    const response = confirm("Are u sure you want to telete this image?");
+    if(response){
+        let imgId = $this.data('id');
+        $.ajax({
+            url: '/images/' + imgId, 
+            type:'DELETE',
+
+        }).done(function(result){
+            $this.removeClass('btn-danger').addClass('btn-success');
+            $this.find('i').removeClass('fa-times').addClass('fa-check');
+            $this.append('<span> Deleted!</span>');
+        });
+    }
+    return false;
+});
