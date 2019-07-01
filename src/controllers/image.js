957efddd66a4d0849246d3451253e1d76f809758
Ignacio <shoik.xtazy@gmail.com>
2019-06-30 23:42:17 -0400

    filter if image exists else redirect to path

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -8,12 +8,19 @@ const { Image, Comment } = require('../models');
 const ctrl = {};
 
 ctrl.index = async (req,res) => {
+    const viewModel = {image: {}, comments:{} };
     const image = await Image.findOne({filename:{$regex: req.params.image_id}});
-    image.views = image.views + 1;
-    await image.save();
-    const comments = await Comment.find({ image_id: image._id });
-    console.log(image);
-    res.render("image", {image, comments});
+    if(image){
+        image.views = image.views + 1;
+        viewModel.image = image;
+        await image.save();
+        const comments = await Comment.find({ image_id: image._id });
+        viewModel.comments = comments;
+        console.log(image);
+        res.render("image", viewModel);
+    }else{
+        res.redirect('/');
+    }
 };
 
 ctrl.create =  (req,res) => {
@@ -63,6 +70,8 @@ ctrl.comment = async (req,res) => {
         newComment.image_id = image._id;
         newComment.save();
         res.redirect('/images/'+image.uniqueId);
+    }else{
+        res.redirect('/');
     }
 
 };
